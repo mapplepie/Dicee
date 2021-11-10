51f68beccef582cab738abce2e5e6edf8d83c843
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-10 19:25:49 +0900

    Add images and Code actions

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceTwo: UIImageView!
+    @IBOutlet weak var diceOne: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 2
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceOne.image = #imageLiteral(resourceName: "DiceFour")
+        diceTwo.image = #imageLiteral(resourceName: "DiceFour")
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        diceOne.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo.jpg"), #imageLiteral(resourceName: "DiceThree.jpg"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceFive")][leftDiceNumber]
+        diceTwo.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo.jpg"), #imageLiteral(resourceName: "DiceThree.jpg"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceFive")][rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
+    
 }
 
