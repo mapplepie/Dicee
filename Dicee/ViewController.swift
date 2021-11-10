fdb8cba4ce2f49d8ffca7ac21a6795458f259dc7
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-10 18:05:22 +0900

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Mai Uchida on 2021/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
